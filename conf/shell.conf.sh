d8fa5e89e7dc430724983cd0f11e947e429c3739
Tariqul Islam <tariqul.islam.rony@gmail.com>
2023-11-01 11:59:44 +0900

    Add the azkaban flow into application

--- conf/shell.conf.sh
+++ conf/shell.conf.sh
@@ -1,4 +1,4 @@
 export _HOST_IP=10.0.2.15:10000
-export _HIVE_USER_NAME=
-export _HIVE_PASSWORD=
-export _REPORT_EXPORT_TO=
+export _HIVE_USER_NAME=hadoop
+export _HIVE_PASSWORD=ronnie01
+export _REPORT_EXPORT_TO=/home/hadoop/app/reports
